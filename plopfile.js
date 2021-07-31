d8ff85351b8360e2696545e2a30ceb1daeb0f6f4
viruspc <pc479684801@outlook.com>
2021-07-31 15:41:53 +0800

    refactor(plopjs): conditonal update index page according to framework

--- plopfile.js
+++ plopfile.js
@@ -1,24 +1,46 @@
-module.exports = function(plop){
-    plop.setGenerator('template', {
-        description: 'create template',
+const frameworks = ['D3', 'Vega', 'Vega-Lite', 'AntV', 'Echarts'];
+
+const InsertPoints = {};
+frameworks.forEach(framework => {
+    InsertPoints[framework] = `<!-- ${framework} Insert Point -->`
+});
+
+module.exports = function (plop) {
+    plop.setGenerator('demo', {
+        description: 'Add new demo',
         prompts: [{
             type: 'input',
             name: 'name',
-            message: 'example name please'
+            message: 'Demo name please'
+        }, {
+            type: 'list',
+            name: 'framework',
+            message: 'Framework please',
+            choices: frameworks,
+            default: frameworks[0]
+            // choices: frameworks.map(framework => ({
+            //     name: framework
+            // })),
+            // default: frameworks[0]
+        },
+        {
+            type: 'input',
+            name: 'source',
+            message: 'Demo source url please'
         }],
-        actions: [{
+        actions: (answers) => [{
             type: 'add',
-            path: 'src/{{kebabCase name}}/index.ts'
+            path: 'src/{{kebabCase framework}}/{{kebabCase name}}/index.js'
         },{
             type: 'add',
-            path: 'src/{{kebabCase name}}/index.html',
-            templateFile: 'templates/example-template.hbs'
+            path: 'src/{{kebabCase framework}}/{{kebabCase name}}/index.html',
+            templateFile: 'templates/demo-template.hbs'
         },{
             type: 'modify',
             path: 'src/index.html',
-            pattern: '<!-- Example Insert Point -->',
-            template: `<li><a href="./{{kebabCase name}}/index.html">{{titleCase name}}</a></li>
-                <!-- Example Insert Point -->`
+            pattern: `<!-- ${answers.framework} Insert Point -->`,
+            template: `<li><a href="./{{kebabCase framework}}/{{kebabCase name}}/index.html">{{titleCase name}}</a></li>
+        <!-- ${answers.framework} Insert Point -->`
         }]
     })
-} 
+}
